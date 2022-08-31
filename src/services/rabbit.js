39c3b0047a568a7bf0491df3af990cd5e69c62d0
Alexander Ditzend <alex@saia.ar>
2022-08-31 17:16:55 -0300

    unique queue

--- src/services/rabbit.js
+++ src/services/rabbit.js
@@ -1,10 +1,43 @@
 // RabbitMQ
 const rascal = require('rascal');
 const { logger } = require('../utils/logger');
-const definitions = require('../rascal-config.json');
 
-const rabbitUrl = process.env.RABBITMQ_HOST || 'amqp://localhost';
-definitions.vhosts.analytics.connection = rabbitUrl;
+const url = process.env.RABBITMQ_HOST || 'amqp://localhost';
+
+const definitions = {
+  vhosts: {
+    analytics: {
+      connection: {
+        url,
+      },
+      exchanges: {
+        analytics_exchange: {
+          assert: true,
+          type: 'direct',
+        },
+      },
+      queues: [
+        'analytics_etl',
+      ],
+      bindings: {
+        analytics: {
+          source: 'analytics_exchange',
+          destination: 'analytics_etl',
+          destinationType: 'queue',
+          bindingKey: 'analytics_route',
+        },
+      },
+      publications: {
+        analytics_publication: {
+          vhost: 'analytics',
+          exchange: 'analytics_exchange',
+          routingKey: 'analytics_route',
+        },
+      },
+    },
+  },
+};
+
 logger.child(definitions).trace('definitions');
 const config = rascal.withDefaultConfig(definitions);
 
